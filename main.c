49836db8f90ac5fc2d894bbd27d9e80c4c575bb1
SamIrung <22ri441@esisalama.org>
2024-06-15 15:37:38 +0100

    Add files via upload
    voila notre fontion principal du code general

--- main.c
+++ main.c
@@ -1,110 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <time.h>
-//LA DESTINATION EST LA DATE DU VOYAGE
-
-typedef struct
-{
-    int jour;
-    int mois;
-    int heure;
-    int annee;
-    char destination[50];
-} Voyage;
-//fonctiion pour ecrire dans le fichier
-void ecrireDansFichier(Voyage v)
-{
-    FILE *f = fopen("reservations.txt", "a");
-    if (f == NULL)
-    {
-        printf("Erreur lors de l'ouverture du fichier.\n");
-        return;
-    }
-    fprintf(f, "Destination: %s, ", v.destination);
-    fprintf(f, "Date: %d/%d/%d, ", v.jour, v.mois, v.annee);
-    fprintf(f, "Heure: %dh00', ", v.heure);
-    fclose(f);
-}
-//fonction principale qui gere  la destinattion et la date
+#include "Application.h"
 int main()
 {
-    Voyage v;
-    int choix;
+    int choix_principal;
+    printf("\t\tBIENVENUE CHEZ SEJ-TRANS \n");
+    printf("\t\t===========================\n\n");
+
     do
     {
-
-        printf("\nChoisissez votre destination:\n1. Kasumbalesa\n2. Kipushi\n3. Likasi\n4. Kolwezi\n5. Quitter\n>> ");
-        scanf("%d", &choix);
-        switch(choix)
+        printf("\t\t1.FAIRE UNE RESERVATION\n\t\t2.VERIFIER SI J'AI UNE RESERVATION\n\t\t3.INFO SUR SEJ-TRANS\n>> ");
+        scanf("%d", &choix_principal);
+        switch(choix_principal)
         {
-        case 1:
-            strcpy(v.destination, "Kasumbalesa");
+        case 1:    //appel aux fonctions
+            Info_Client();
+            Destination();
+            Siege_disponible();
+            Paiement();
+
+            int reponse;
+            printf("\n\nvoulez-vous faire une autre reservation\n0. NON\n1. OUI\n>> ");
+            scanf("%d", &reponse);
+            while(1)//boucle pour demander a l'utilisateur veut faire une autre reservation
+            {
+                switch(reponse)
+                {
+                case 1:
+                    Info_Client();
+                    Destination();
+                    Siege_disponible();
+                    Paiement();
+                    printf("\n\nvoulez-vous faire une autre reservation\n0. NON\n1. OUI\n>> ");
+                    scanf("%d", &reponse);
+                    break;
+                case 0:
+                    printf("Reservation fait avec succes!");
+                    exit(0);
+                default:
+                    printf("mauvaise saisi ,");
+                    printf("\n\nvoulez-vous faire une autre reservation\n0. NON\n1. OUI\n>> ");
+                    scanf("%d", &reponse);
+                    continue;
+                }
+            }
             break;
         case 2:
-            strcpy(v.destination, "Kipushi");
-            break;
+            Veification();
+            exit(0);
         case 3:
-            strcpy(v.destination, "Likasi");
-            break;
-        case 4:
-            strcpy(v.destination, "Kolwezi");
-            break;
-        case 5:
-            //Annuler();
-            //break;
+            A_propos() ;
             exit(0);
         default:
-            printf("Choix invalide.\n");
+            printf("Erreur de saisie!\n");
             continue;
         }
-        if(choix == 1 || choix == 2 || choix == 3 || choix == 4 || choix == 5)
-        {
-            break;
-        }
     }
     while(1);
-
-    printf("Entrez le jour du voyage: ");
-    scanf("%d", &(v.jour));
-    printf("Entrez le mois du voyage: ");
-    scanf("%d", &(v.mois));
-    do
-    {
-        printf("Choisissez l'heure d'embarquement :\n1. 9h\n2. 12h\n3. 15h\n4. Quitter\n>> ");
-        scanf("%d", &choix);
-        switch(choix)
-        {
-        case 1:
-            v.heure = 9;
-            break;
-        case 2:
-            v.heure = 12;
-            break;
-        case 3:
-            v.heure = 15;
-            break;
-        case 4:
-            //Annuler();
-           // break;
-           exit(0);
-        default:
-            printf("Choix invalide.\n");
-            return 0;
-        }
-
-        if(choix == 1 || choix == 2 || choix == 3 || choix == 4)
-        {
-            break;
-        }
-    }
-    while(1);
-
-    //Genere automatiique l'année actuelle
-    time_t t = time(NULL);
-    struct tm tm = *localtime(&t);
-    v.annee = tm.tm_year + 1900;
-    //Appel a la foncion, pour ecrire dans le fichier
-    ecrireDansFichier(v);
     return 0;
 }
